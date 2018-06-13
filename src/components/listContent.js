3785c89558ef52f4a752b3dd52b9f451fcc1abe5
fsegall <segall.felipe@gmail.com>
2018-06-13 10:54:15 -0300

    Insert roundend corners for list container

--- src/components/listContent.js
+++ src/components/listContent.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function ListContent(props) {
   return (
-    <div className="pt-3 mb-2 border border-secondary border-white border-bottom-0">
+    <div className="pt-3 mb-2 border border-secondary border-white border-bottom-0 rounded">
       <h1 className="ml-2">{props.status}</h1>
 
       <ul className="list-group mb-5">{props.renderList}</ul>
