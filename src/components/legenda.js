3c95d17b7df63d74b51602531eccc34bd2365323
fsegall <segall.felipe@gmail.com>
2018-06-14 10:54:30 -0300

    Use flexbox properties

---
+++ src/components/legenda.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Legenda(props) {
+  return (
+    <ul>
+      <li>
+        <span />
+      </li>
+      <li>
+        <span />
+      </li>
+      <li>
+        <span />
+      </li>
+      <li>
+        <span />
+      </li>
+      <li>
+        <span />
+      </li>
+      <li>
+        <span />
+      </li>
+      <li>
+        <span />
+      </li>
+      <li>
+        <span />
+      </li>
+    </ul>
+  );
+}
--- src/components/statusBar.js
+++ src/components/statusBar.js
@@ -10,26 +10,27 @@ function StatusBar(props) {
 
   return (
     <div>
-      <div className="d-inline float-right">
-        <button className="btn btn-secondary m-1">My Articles</button>
+      <div className="text-right d-flex justify-content-end flex-wrap">
+        <button className="btn btn-secondary mx-1">My Articles</button>
         <button className="btn btn-secondary">Authors</button>
       </div>
-
-      <div className="d-flex justify-content-end text-white font-bold text-center">
-        <div className="bg-secondary mt-1">
-          <p className="p-2">{`Total ${articlesTotal}`}</p>
-        </div>
-        <div className="bg-danger mt-1">
-          <p className="p-2">{`Private ${props.articlesPrivate}`}</p>
-        </div>
-        <div className="bg-warning mt-1">
-          <p className="p-2">{`Not Reviewed ${props.articlesNotReviewed}`}</p>
-        </div>
-        <div className="bg-success mt-1">
-          <p className="p-2">{`Reviewed ${props.articlesReviewed}`}</p>
-        </div>
-        <div className="bg-primary mt-1">
-          <p className="p-2">{`Published ${props.articlesPublished}`}</p>
+      <div className="d-flex flex-wrap justify-content-start text-white font-bold text-center">
+        <div className="d-flex flex-wrap text-white font-bold text-center">
+          <div className="bg-secondary mt-1 box">
+            <p className="p-2">{`Total ${articlesTotal}`}</p>
+          </div>
+          <div className="bg-danger mt-1 box">
+            <p className="p-2">{`Private ${props.articlesPrivate}`}</p>
+          </div>
+          <div className="bg-warning mt-1 box">
+            <p className="p-2">{`Not Reviewed ${props.articlesNotReviewed}`}</p>
+          </div>
+          <div className="bg-success mt-1 box">
+            <p className="p-2">{`Reviewed ${props.articlesReviewed}`}</p>
+          </div>
+          <div className="bg-primary mt-1 box">
+            <p className="p-2">{`Published ${props.articlesPublished}`}</p>
+          </div>
         </div>
       </div>
     </div>
