dd4c0230a41774f02e78df9b11543bc08107efdd
fsegall <segall.felipe@gmail.com>
2018-06-15 16:35:03 -0300

    Make statusBar collapse with click

--- src/components/articleItem.js
+++ src/components/articleItem.js
@@ -4,12 +4,15 @@ import Locked from "./locked";
 
 function ArticleItem(props) {
   const article = props.article;
+  const classAnimated = article.publishDate
+    ? "badge badge-primary float-right animated slideInRight"
+    : "";
 
   return (
     <li className="list-group-item list-group-item-action">
       <Icons iconList={article.icons} />
-      <span className="badge badge-primary float-right">
-        {article.publishDate}
+      <span className={classAnimated}>
+        <span className="">{article.publishDate}</span>
       </span>
       <div className="m-2">{article.headline}</div>
       <strong className="ml-2">{`Escrita por: ${article.author}`}</strong>
--- src/components/statusBar.js
+++ src/components/statusBar.js
@@ -4,18 +4,31 @@ class StatusBar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      extended: false,
-      hidden: true
+      extended: true,
+      hidden: false
     };
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick(e) {
+    console.log(e.target);
+    this.setState({
+      extended: !this.state.extended,
+      hidden: !this.state.hidden
+    });
+
+    console.log(this.state);
   }
 
   render() {
-    console.log(this.props);
     const articlesTotal =
       this.props.articlesPrivate +
       this.props.articlesNotReviewed +
       this.props.articlesReviewed +
       this.props.articlesPublished;
+
+    const extended = this.state.extended;
+    const hidden = this.state.hidden;
 
     return (
       <div>
@@ -24,36 +37,57 @@ class StatusBar extends Component {
           <button className="btn btn-default m-1">Authors</button>
           <button className="btn btn-warning m-1">Locked</button>
         </div>
+
         <div className="row">
           <div className="d-flex flex-wrap text-white font-bold text-center col-md-6">
             <div className="bg-secondary box p-2">
-              <p className="">Total</p>
-              <div className="">{articlesTotal}</div>
+              <div className={!hidden ? "" : "d-none"}>
+                <p className="">Total</p>
+                <div className="">{articlesTotal}</div>
+              </div>
             </div>
 
             <div className="bg-danger box p-2">
-              <p className="">Private</p>
-              <div>{this.props.articlesPrivate}</div>
+              <div className={!hidden ? "" : "d-none"}>
+                <p className="">Private</p>
+                <div>{this.props.articlesPrivate}</div>
+              </div>
             </div>
 
             <div className="bg-warning box p-2">
-              <p className="">Edit</p>
-              <div className="">{this.props.articlesNotReviewed}</div>
+              <div className={!hidden ? "" : "d-none"}>
+                <p className="">Edit</p>
+                <div className="">{this.props.articlesNotReviewed}</div>
+              </div>
             </div>
 
             <div className="bg-success box p-2">
-              <p className="">Reviewed</p>
-              <div className="">{this.props.articlesReviewed}</div>
+              <div className={!hidden ? "" : "d-none"}>
+                <p className="">Reviewed</p>
+                <div className="">{this.props.articlesReviewed}</div>
+              </div>
             </div>
 
             <div className="bg-primary box p-2">
-              <p className="">Published</p>
-              <div className="">{this.props.articlesPublished}</div>
+              <div className={!hidden ? "" : "d-none"}>
+                <p className="">Published</p>
+                <div className="">{this.props.articlesPublished}</div>
+              </div>
             </div>
+
+            <span
+              onClick={e => this.onClick(e)}
+              className={
+                this.state.extended
+                  ? "fas fa-minus-circle text-secondary ml-2"
+                  : "fas fa-plus-circle text-secondary ml-2"
+              }
+            />
           </div>
         </div>
       </div>
     );
   }
 }
+
 export default StatusBar;
