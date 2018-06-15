1764d0f69c6938cdd83ed0b24c831e2f7cbd4659
fsegall <segall.felipe@gmail.com>
2018-06-15 14:03:53 -0300

    Give state to statusBar and include material design bootstrap to project

--- src/components/statusBar.js
+++ src/components/statusBar.js
@@ -1,49 +1,59 @@
-import React from "react";
+import React, { Component } from "react";
 
-function StatusBar(props) {
-  console.log(props);
-  const articlesTotal =
-    props.articlesPrivate +
-    props.articlesNotReviewed +
-    props.articlesReviewed +
-    props.articlesPublished;
+class StatusBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      extended: false,
+      hidden: true
+    };
+  }
 
-  return (
-    <div>
-      <div className="text-right">
-        <button className="btn btn-secondary m-1">My Articles</button>
-        <button className="btn btn-secondary m-1">Authors</button>
-        <button className="btn btn-warning m-1">Locked</button>
-      </div>
-      <div className="row">
-        <div className="d-flex flex-wrap text-white font-bold text-center col-md-6">
-          <div className="bg-secondary box p-2">
-            <p className="">Total</p>
-            <div className="">{articlesTotal}</div>
-          </div>
+  render() {
+    console.log(this.props);
+    const articlesTotal =
+      this.props.articlesPrivate +
+      this.props.articlesNotReviewed +
+      this.props.articlesReviewed +
+      this.props.articlesPublished;
 
-          <div className="bg-danger box p-2">
-            <p className="">Private</p>
-            <div>{props.articlesPrivate}</div>
-          </div>
+    return (
+      <div>
+        <div className="text-right">
+          <button className="btn btn-default m-1">My Articles</button>
+          <button className="btn btn-default m-1">Authors</button>
+          <button className="btn btn-warning m-1">Locked</button>
+        </div>
+        <div className="row">
+          <div className="d-flex flex-wrap text-white font-bold text-center col-md-6">
+            <div className="bg-secondary box p-2">
+              <p className="">Total</p>
+              <div className="">{articlesTotal}</div>
+            </div>
 
-          <div className="bg-warning box p-2">
-            <p className="">Edit</p>
-            <div className="">{props.articlesNotReviewed}</div>
-          </div>
+            <div className="bg-danger box p-2">
+              <p className="">Private</p>
+              <div>{this.props.articlesPrivate}</div>
+            </div>
 
-          <div className="bg-success box p-2">
-            <p className="">Reviewed</p>
-            <div className="">{props.articlesReviewed}</div>
-          </div>
+            <div className="bg-warning box p-2">
+              <p className="">Edit</p>
+              <div className="">{this.props.articlesNotReviewed}</div>
+            </div>
 
-          <div className="bg-primary box p-2">
-            <p className="">Published</p>
-            <div className="">{props.articlesPublished}</div>
+            <div className="bg-success box p-2">
+              <p className="">Reviewed</p>
+              <div className="">{this.props.articlesReviewed}</div>
+            </div>
+
+            <div className="bg-primary box p-2">
+              <p className="">Published</p>
+              <div className="">{this.props.articlesPublished}</div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 export default StatusBar;
