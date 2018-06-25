c37defed50d9d641b5f6d2595a94501c1a97f124
fsegall <segall.felipe@gmail.com>
2018-06-25 15:31:34 -0300

    Include color status class for badge with counters

--- src/components/listContent.js
+++ src/components/listContent.js
@@ -3,11 +3,33 @@ import React from "react";
 // Renders a container for a list and its article items
 
 const ListContent = props => {
+  let articleStatus = props.status;
+  console.log(props.status);
+  let bg;
+
+  switch (articleStatus) {
+    case "Private":
+      bg = "bg-danger";
+      break;
+    case "Not Reviewed":
+      bg = "bg-warning";
+      break;
+    case "Reviewed":
+      bg = "bg-success";
+      break;
+    case "Published":
+      bg = "bg-primary";
+      break;
+    default:
+      bg = "";
+  }
+
   return (
     <div className="pt-3 mb-2 border border-secondary border-white border-bottom-0 rounded">
       <h1 className="ml-2">
         {props.status}
-        <span className="badge float-right text-secondary mr-2">
+
+        <span className={`badge ${bg} float-right text-light mr-2`}>
           {props.populateList.length}
         </span>
       </h1>
