71aff9c7ad6e22cc63e9c0b2d666132c29e7a440
fsegall <segall.felipe@gmail.com>
2018-06-25 14:59:18 -0300

    Add counter for each list

--- src/components/listContent.js
+++ src/components/listContent.js
@@ -5,7 +5,12 @@ import React from "react";
 const ListContent = props => {
   return (
     <div className="pt-3 mb-2 border border-secondary border-white border-bottom-0 rounded">
-      <h1 className="ml-2">{props.status}</h1>
+      <h1 className="ml-2">
+        {props.status}
+        <span className="badge float-right text-secondary mr-2">
+          {props.populateList.length}
+        </span>
+      </h1>
 
       <ul className="list-group mb-5">{props.populateList}</ul>
     </div>
