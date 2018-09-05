a391132325fc63282c97155838a3ef0c22a759fa
fsegall <segall.felipe@gmail.com>
2018-09-05 17:30:03 -0300

    Start search articles by title feature

--- src/components/lists.js
+++ src/components/lists.js
@@ -1,54 +1,93 @@
-import React from "react";
+import React, { Component } from "react";
 import { renderList } from "./renderList";
 import StatusBar from "./statusBar";
 
 // Renders all lists of articles calling renderList helper function and the StatusBar with list item counters
 
-const Lists = props => {
-  const statusPrivate = props.workStatus[0];
-  const privateList = props.articles.filter(article => article.private);
+class Lists extends Component {
 
-  const articlesPrivate = privateList.length;
+  constructor(props) {
+    super(props)
+    this.onchange = this.onChange.bind(this);
+  }
 
-  const statusReview = props.workStatus[1];
-  const reviewList = props.articles.filter(article => !article.reviewed);
+  state = {
+    articles: this.props.articles
+  }
 
-  const articlesNotReviewed = reviewList.length;
 
-  const statusReviewed = props.workStatus[2];
-  const reviewedList = props.articles.filter(
-    article => article.reviewed && !article.publishDate
-  );
 
-  const articlesReviewed = reviewedList.length;
+  onChange = (e) => {
 
-  const statusPublish = props.workStatus[3];
-  const publishList = props.articles.filter(article => article.publishDate);
+    let updatedList = this.state.articles.filter(article =>
 
-  const articlesPublished = publishList.length;
 
-  return (
-    <div className="container-fluid">
-      <div className="row">
-        <div col-md-12>
-          <StatusBar
-            articlesPrivate={articlesPrivate}
-            articlesNotReviewed={articlesNotReviewed}
-            articlesReviewed={articlesReviewed}
-            articlesPublished={articlesPublished}
-          />
+      article.headline.toLowerCase().search(e.target.value.toLowerCase()) !== -1
+
+    )
+
+    console.log('updatedList', updatedList);
+
+    return this.setState({
+      articles: e.target.value ? updatedList : this.props.articles
+    })
+
+  }
+
+  render() {
+    // Article status, filters for lists and list count
+
+    const statusPrivate = this.props.workStatus[0]
+    const privateList = this.state.articles.filter(article => article.private)
+    const articlesPrivate = privateList.length
+
+
+    const statusReview = this.props.workStatus[1]
+    const reviewList = this.state.articles.filter(article => !article.reviewed)
+    const articlesNotReviewed = reviewList.length
+
+    const statusReviewed = this.props.workStatus[2]
+    const reviewedList = this.state.articles.filter(
+      article => article.reviewed && !article.publishDate
+    )
+    const articlesReviewed = reviewedList.length
+
+    const statusPublish = this.props.workStatus[3]
+    const publishList = this.state.articles.filter(article => article.publishDate)
+    const articlesPublished = publishList.length
+
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div col-md-12>
+            <StatusBar
+              articlesPrivate={articlesPrivate}
+              articlesNotReviewed={articlesNotReviewed}
+              articlesReviewed={articlesReviewed}
+              articlesPublished={articlesPublished}
+            />
+          </div>
+
+          <form className="searchTitle ml-3">
+            <label>
+              Filtrar por título:
+            <input
+                className="ml-2"
+                type="text"
+                onChange={this.onChange}
+              />
+            </label>
+          </form>
+        </div>
+        <div className="pt-3 row no-gutters">
+          <div className="col-lg-3">{renderList(statusPrivate, privateList)}</div>
+          <div className="col-lg-3">{renderList(statusReview, reviewList)}</div>
+          <div className="col-lg-3">{renderList(statusReviewed, reviewedList)}</div>
+          <div className="col-lg-3">{renderList(statusPublish, publishList)}</div>
         </div>
       </div>
-      <div className="pt-3 row no-gutters">
-        <div className="col-lg-3">{renderList(statusPrivate, privateList)}</div>
-        <div className="col-lg-3">{renderList(statusReview, reviewList)}</div>
-        <div className="col-lg-3">
-          {renderList(statusReviewed, reviewedList)}
-        </div>
-        <div className="col-lg-3">{renderList(statusPublish, publishList)}</div>
-      </div>
-    </div>
-  );
+    );
+  };
 };
 
 export default Lists;
